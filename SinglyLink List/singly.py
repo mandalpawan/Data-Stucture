71b8ef39605f9f0f34ddafe4a038596d2faed26a
mandalpawan <pawankrmandal121@gmail.com>
2020-03-29 19:29:03 +0530

    Modified Dublicate Data

--- SinglyLink List/singly.py
+++ SinglyLink List/singly.py
@@ -88,6 +88,23 @@ class singlyLinkList:
                 prev = cur
             cur = prev.next
 
+    def count_occur(self,data):
+        count =0
+        cur = self.head
+        while cur:
+            if cur.value == data:
+                count +=1
+            cur = cur.next
+        return count
+    
+    def count_recursive(self,node,data):
+        if not node:
+            return 0
+        if node.value == data:
+            return 1+ self.count_recursive(node.next,data)
+        else:
+            return  self.count_recursive(node.next,data)
+
 mylist = singlyLinkList()
 mylist.appendEnd(1)
 mylist.appendEnd(2)
@@ -98,8 +115,7 @@ mylist.appendEnd(6)
 mylist.appendEnd(1)
 mylist.print_list()
 print()
-mylist.removeDublicate()
-mylist.print_list()
+print(mylist.count_occur(1))
 
 
 
