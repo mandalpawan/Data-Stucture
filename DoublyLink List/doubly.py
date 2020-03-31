e3c2d544e92b00136ee57702a29f47c5181a11d8
mandalpawan <pawankrmandal121@gmail.com>
2020-03-31 15:25:17 +0530

    add More feature

--- DoublyLink List/doubly.py
+++ DoublyLink List/doubly.py
@@ -1,80 +1,126 @@
 class Node:
-    def __init__(self,data):
-        self.data = data 
+    def __init__(self, data):
+        self.data = data
         self.next = None
         self.prev = None
 
-class DoblyLinkList:
+
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self,data):
-        newNode = Node(data)
+    def append(self, data):
         if self.head is None:
-            self.head = newNode
-            newNode.next = None
-            newNode.prev = None
+            new_node = Node(data)
+            new_node.prev = None
+            self.head = new_node
         else:
-            cur_node = self.head
-            while cur_node.next:
-                cur_node = cur_node.next
-            cur_node.next = newNode
-            newNode.prev = cur_node
-            newNode.next = None
+            new_node = Node(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+            new_node.prev = cur
+            new_node.next = None
 
-    def prepend(self,data):
-        newNode = Node(data)
+    def prepend(self, data):
         if self.head is None:
-            self.head = newNode
-            newNode.next = None
-            newNode.prev = None
+            new_node = Node(data)
+            new_node.prev = None 
+            self.head = new_node
         else:
-            cur_node = self.head
-            cur_node.prev = newNode
-            newNode.next = cur_node
-            newNode.prev = None
-            self.head = newNode
+            new_node = Node(data)
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            new_node.prev = None
+
+    def print_list(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
 
     def add_after_node(self, key, data):
         cur = self.head
         while cur:
             if cur.next is None and cur.data == key:
                 self.append(data)
+                return 
             elif cur.data == key:
                 new_node = Node(data)
-                nxt = cur.next 
+                nxt = cur.next
                 cur.next = new_node
                 new_node.next = nxt
+                new_node.prev = cur
                 nxt.prev = new_node
             cur = cur.next
 
     def add_before_node(self, key, data):
-        cur = self.head 
+        cur = self.head
         while cur:
             if cur.prev is None and cur.data == key:
                 self.prepend(data)
+                return
             elif cur.data == key:
                 new_node = Node(data)
                 prev = cur.prev
                 prev.next = new_node
                 cur.prev = new_node
                 new_node.next = cur
+                new_node.prev = prev
             cur = cur.next
 
-  
-    def print_list(self):
-        cur_node = self.head
-        while cur_node:
-            print(cur_node.data)
-            cur_node = cur_node.next
+    def delete(self, key):
+        cur = self.head
+        while cur:
+            if cur.data == key and cur == self.head:
+                # Case 1:
+                if not cur.next:
+                    cur = None 
+                    self.head = None
+                    return
+
+                # Case 2:
+                else:
+                    nxt = cur.next
+                    cur.next = None 
+                    nxt.prev = None
+                    cur = None
+                    self.head = nxt
+                    return 
+
+            elif cur.data == key:
+                # Case 3:
+                if cur.next:
+                    nxt = cur.next 
+                    prev = cur.prev
+                    prev.next = nxt
+                    nxt.prev = prev
+                    cur.next = None 
+                    cur.prev = None
+                    cur = None
+                    return
+
+                # Case 4:
+                else:
+                    prev = cur.prev 
+                    prev.next = None 
+                    cur.prev = None 
+                    cur = None 
+                    return 
+            cur = cur.next
 
 
-dllist = DoblyLinkList()
+dllist = DoublyLinkedList()
+dllist.append(1)
 dllist.append(2)
 dllist.append(3)
-dllist.append(5)
-dllist.prepend(1)
-dllist.add_after_node(3,4)
-dllist.add_before_node(5,4.4)
+dllist.append(4)
 dllist.print_list()
+dllist.delete(1)
+dllist.delete(6)
+dllist.delete(4)
 
+dllist.delete(3)
+dllist.print_list()
