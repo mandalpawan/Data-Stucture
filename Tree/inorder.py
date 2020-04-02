599ea3692b44088c7369bae6c6a582af24c094f7
mandalpawan <pawankrmandal121@gmail.com>
2020-04-02 13:29:34 +0530

    inoreder

--- Tree/inorder.py
+++ Tree/inorder.py
@@ -35,42 +35,11 @@ class BinaryTree(object):
         self.root = Node(root)
 
     def print_tree(self, traversal_type):
-        if traversal_type == "preorder":
-            return self.preorder_print(tree.root, "")
-        elif traversal_type == "inorder":
-            return self.inorder_print(tree.root, "")
-        elif traversal_type == "postorder":
-            return self.postorder_print(tree.root, "")
-        elif traversal_type == "levelorder":
+        if traversal_type == "levelorder":
             return self.levelorder_print(tree.root)
-
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
             return False
-
-    def preorder_print(self, start, traversal):
-        """Root->Left->Right"""
-        if start:
-            traversal += (str(start.value) + "-")
-            traversal = self.preorder_print(start.left, traversal)
-            traversal = self.preorder_print(start.right, traversal)
-        return traversal
-
-    def inorder_print(self, start, traversal):
-        """Left->Root->Right"""
-        if start:
-            traversal = self.inorder_print(start.left, traversal)
-            traversal += (str(start.value) + "-")
-            traversal = self.inorder_print(start.right, traversal)
-        return traversal
-
-    def postorder_print(self, start, traversal):
-        """Left->Right->Root"""
-        if start:
-            traversal = self.inorder_print(start.left, traversal)
-            traversal = self.inorder_print(start.right, traversal)
-            traversal += (str(start.value) + "-")
-        return traversal
 
     def levelorder_print(self, start):
         if start is None:
