fcd894627f28d09aa387dfc25e2833ea6e991775
mandalpawan <pawankrmandal121@gmail.com>
2020-04-05 14:07:08 +0530

    delete function

--- Tree/bst.py
+++ Tree/bst.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,data= None):
+    def __init__(self,data):
         self.data = data
         self.left = None
         self.right= None
@@ -51,6 +51,38 @@ class BinaryTree:
         else:
             self._inorder(self.root)
     
+    def minValue(self,node):
+        cur_node = node
+        while cur_node.left:
+            cur_node = cur_node.left
+        return cur_node
+    
+    def remove(self,key):
+        if self.root:
+            self._remove(self.root,key)
+
+    def _remove(self,cur_node,key):
+        node = cur_node
+        if node is None:
+            return False
+        if key< node.data:
+            node.left = self._remove(node.left,key)
+        elif(key>node.data):
+            node.right = self._remove(node.right,key)
+        else:
+            if node.left is None:
+                temp = cur_node.right
+                root = None
+                return temp
+            elif node.right is None:
+                temp = cur_node.left
+                root = None
+                return temp
+            temp = self.minValue(node.right)
+            node.data = temp.data
+            node.right = self._remove(node.right,temp.data)
+        return node
+
     def _inorder(self,cur_node):
         if cur_node:
             self._inorder(cur_node.left)
@@ -59,12 +91,19 @@ class BinaryTree:
 
 
 bst = BinaryTree()
-bst.insert(4)
-bst.insert(2)
-bst.insert(8)
+bst.insert(15)
 bst.insert(5)
-bst.insert(10)
+bst.insert(8)
+bst.insert(6)
+bst.insert(24)
+bst.insert(19)
+bst.insert(30)
+bst.insert(21)
 bst.inorder()
+bst.remove(15)
+print("after delete")
+bst.inorder()
+
 
 
         
