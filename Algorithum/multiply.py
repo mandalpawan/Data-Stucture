2af95e448cb0d951afd77f5d540897d1c3f9112a
mandalpawan <pawankrmandal121@gmail.com>
2020-04-10 19:14:37 +0530

    Multiply two number using recursion:

---
+++ Algorithum/multiply.py
@@ -0,0 +1,13 @@
+x = 500
+y = 2000
+
+
+def recursive_multiply(x, y):
+    if x < y:
+        return recursive_multiply(y, x)
+    if y == 0:
+        return 0
+    return x + recursive_multiply(x, y-1)
+
+print(x * y)
+print(recursive_multiply(x, y))
